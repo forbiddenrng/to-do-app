936bbc01ed84f0e7ee169953702322c499e05b1a
Antek <antek.gawron@gmail.com>
2020-09-07 21:59:58 +0200

    Updates

---
+++ precache-manifest.a9a1c2b36f0ad0c4b2fa708dd46b56d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcd8a03752e9694e4450",
+    "url": "/to-do-app/static/css/main.a1577c14.chunk.css"
+  },
+  {
+    "revision": "dcd8a03752e9694e4450",
+    "url": "/to-do-app/static/js/main.79224284.chunk.js"
+  },
+  {
+    "revision": "6cf4045563d5e41dd4b3",
+    "url": "/to-do-app/static/js/runtime~main.01361c46.js"
+  },
+  {
+    "revision": "e84cceccc2841b85d993",
+    "url": "/to-do-app/static/js/2.f4426b14.chunk.js"
+  },
+  {
+    "revision": "b4d018c5e05d535ed83c50b596050f31",
+    "url": "/to-do-app/index.html"
+  }
+];
